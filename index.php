b223654c2954b2c90c10a7fd2abd74f82843b118
rockkw <rock.whitney.private@gmail.com>
2018-10-30 14:16:34 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,6 @@
   
  <?php echo "Today is: " . date("Y/m/d") . "<br>"; ?>
   
-  Hostname is:
-<?php
-echo gethostname(); // may output e.g,: sandie
-
-// Or, an option that also works before PHP 5.3
-echo php_uname('n'); // may output e.g,: sandie
-?>
 
  </body>
 </html>
