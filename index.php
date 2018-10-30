a38069b90949832ceec66492b0a48103be672a87
rockkw <rock.whitney.private@gmail.com>
2018-10-30 13:15:27 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,15 @@
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
   
- <?php echo "Today is " . date("Y/m/d") . "<br>"; ?>
+ <?php echo "Today is: " . date("Y/m/d") . "<br>"; ?>
   
+  Hostname is:
+<?php
+echo gethostname(); // may output e.g,: sandie
+
+// Or, an option that also works before PHP 5.3
+echo php_uname('n'); // may output e.g,: sandie
+?>
+
  </body>
 </html>
